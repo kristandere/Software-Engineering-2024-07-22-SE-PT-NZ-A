b56f300aa32551dc6b69546efcf41897e2489534
kristandere <kristandere69@gmail.com>
2024-07-31 19:33:25 +1200

    my new commits

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Software-Engineering-2024-07-22-SE-PT-NZ-A
 Repository to save all the code samples and exercises
+
+hello new text
---
+++ module1/exercise.js
@@ -0,0 +1,13 @@
+function colorize() {
+    var element = document.getElementsByTagName("heading1")[0];
+    element.style.backgroundColor = 'blue';
+}
+
+function hi()
+{
+document.getElementById("column1").innerHTML="It changed";
+}
+function hello()
+{
+document.getElementById("column2").innerHTML="Good";
+}
