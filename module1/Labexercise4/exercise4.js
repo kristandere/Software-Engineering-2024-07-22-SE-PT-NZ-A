b5b3661ce5a4abc7b9da1de6a4c959552f51d458
kristandere <kristandere69@gmail.com>
2024-08-01 21:41:53 +1200

    New files for exercises

---
+++ module1/Labexercise4/exercise4.js
@@ -0,0 +1,16 @@
+let a = 5;
+let b = 6;
+
+function sum(a, b){
+    console.log(a - b)
+    console.log(a + b)
+    console.log(a / b)
+    console.log(a * b)
+}
+sum(a, b)
+
+function sayHello(){
+    console.log("hello"+"kris")
+}
+
+sayHello()
